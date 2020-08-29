1fab1bc6e78f9bb5eb3cecb94f2bbf4fc62e78b2
xiaoshaoxing <xiaoshaoxing@adups.com>
2020-08-29 23:36:59 +0800

    时间窗口的解题技巧: 双指针，右动左不动，左动右不动，判断用matchCount

--- 76.最小覆盖子串.ts
+++ 76.最小覆盖子串.ts
@@ -22,21 +22,35 @@ function minWindow(s: string, t: string): string {
   }
 
   let left = 0, right = 0
+  // 记录已经匹配到的字符个数
+  let matchCount = 0
+  let needSize = Object.keys(needChars).length
 
   while (right < s.length) {
     let c = s[right]
     window[c] = window[c] ? window[c] + 1 : 1
+
+    if (window[c] === needChars[c]) {
+      matchCount++
+    }
+
     right++
 
-    while (windowNeedShrink(needChars, window)) {
+    while (matchCount === needSize) {
       // 当找到一个窗口后，需要更新最小窗口的起止
       if (right - left < minLength) {
         minStart = left
         minLength = right - left
       }
 
+      let deletedChar = s[left]
       // 减掉最左边的元素
-      window[s[left]]--
+      window[deletedChar]--
+
+      if (window[deletedChar] < needChars[deletedChar]) {
+        matchCount--
+      }
+
       left++
     }
   }
@@ -44,18 +58,6 @@ function minWindow(s: string, t: string): string {
   return minLength === Number.POSITIVE_INFINITY ? "" : s.substr(minStart, minLength)
 };
 
-function windowNeedShrink(needChars: any, window: any) {
-  let matchCount = 0
-
-  for (const c in needChars) {
-    if (window[c] >= needChars[c]) {
-      matchCount++
-    }
-  }
-
-  return matchCount === Object.keys(needChars).length
-}
-
-// console.log(minWindow("a", "b"))
+// console.log(minWindow("ADOBECODEBANC", "ABC"))
 // @lc code=end
 
