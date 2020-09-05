62e55db26f6bc2beda0f3a4304f266e0f5f22102
xiaoshaoxing <xiaoshaoxing@adups.com>
2020-09-06 00:35:13 +0800

    105题的解法是有思想存在的

---
+++ 105.从前序与中序遍历序列构造二叉树.java
@@ -0,0 +1,54 @@
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=105 lang=java
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    int pre = 0, in = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(preorder, inorder, Integer.MAX_VALUE);
+    }
+
+    public TreeNode helper(int[] preorder, int[] inorder, int stop) {
+        if (pre == preorder.length) {
+            return null;
+        }
+
+        if (inorder[in] == stop) {
+            in++;
+            return null;
+        }
+
+        int rootVal = preorder[pre];
+        pre++;
+        TreeNode root = new TreeNode(rootVal);
+        root.left = helper(preorder, inorder, rootVal);
+        root.right = helper(preorder, inorder, stop);
+
+        return root;
+    }
+}
+// @lc code=end
+
