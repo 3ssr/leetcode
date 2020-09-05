9941197c3a485f7e9b8e64548cde77f715325f26
xiaoshaoxing <xiaoshaoxing@adups.com>
2020-09-05 14:36:12 +0800

    98题，记住判断二叉搜索树是否合法，对于右子树需要比较当前的max值，对于左子树需要比较当前的min值

---
+++ 98.验证二叉搜索树.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=java
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean helper(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+}
+// @lc code=end
+
