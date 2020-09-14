9742e9d563757f5054493ad655764d0e43f442c4
xiaoshaoxing <xiaoshaoxing@adups.com>
2020-09-15 00:19:05 +0800

    222题目利用二分法找到最后一层元素最右边节点的位置

---
+++ 222.完全二叉树的节点个数.java
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=java
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    int count = 0;
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        // 树的深度
+        int depth = depth(root);
+        // 倒数第二层的深度
+        int depth2 = depth - 1;
+
+        int start = 1, end = (1 << depth2);
+
+        while (start <= end) {
+            int mid = start + ((end - start) >> 1);
+            if (isExistNode(root, mid, depth2)) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        // 除去最后一层的节点数 + 最后一层的节点数
+        return (1 << depth2) - 1 + (start - 1);
+    }
+
+    /**
+     * 判断树的最后一层的index位置上是否有节点
+     * @param root
+     * @param index
+     * @param depth
+     * @return
+    */
+    private boolean isExistNode(TreeNode root, int index, int depth) {
+        // 找到index节点
+        TreeNode curr = root;
+        while (depth > 0) {
+            // 分割线的位置在2的depth次方除以2的位置
+            int splitLine = ((1 << depth) >> 1);
+
+            if (index > splitLine) {
+                curr = curr.right;
+                index -= splitLine;
+            } else {
+                curr = curr.left;
+            }
+
+            depth--;
+        }
+
+        return curr != null;
+    }
+
+    public int depth(TreeNode root) {
+        int levels = 0;
+    	while (root != null) {
+            root = root.left;
+            levels += 1;
+        }
+
+        return levels;
+    }
+}
+// @lc code=end
+
